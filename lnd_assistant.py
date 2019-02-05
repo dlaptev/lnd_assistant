ffb84580eb93728e78f603b7a4d23a9313b3f4fb
root <laptev.d.a@gmail.com>
2019-02-05 23:10:58 +0000

    newly opened channels table

--- lnd_assistant.py
+++ lnd_assistant.py
@@ -1,39 +1,68 @@
-from __future__ import print_function
 from collections import defaultdict
 import json
 import os
+import pickle
 import time
 
 
-def outgoing_channel(ch):
-  # TODO: can probably be done for closed channels as well through
-  #       settled_balance and time_locked_balance.
-  return ((float(ch['local_balance']) > 0 and
-           float(ch['total_satoshis_received']) == 0) or
-          (float(ch['total_satoshis_sent']) >
-           float(ch['total_satoshis_received'])))
-
-
-def BTC(satoshi_as_string):
+def sat_to_btc(satoshi_as_string):
   return int(satoshi_as_string) * 1e-8
 
 
-def format_satoshi(satoshi_as_string):
-  if type(satoshi_as_string) == int:
-    satoshi_as_string = str(satoshi_as_string)
-  temp = []
-  for i in range(1, len(satoshi_as_string) + 1):
-    temp.append(satoshi_as_string[-i])
-    if i % 3 == 0 and i != len(satoshi_as_string):
-      temp.append('\'')
-  return ''.join(temp[::-1])
+class Printer:
+  @staticmethod
+  def bprint(str):
+    print('\033[01m%s\033[00m' % (str))
+
+  @staticmethod
+  def cprint(str):
+    print('\033[96m%s\033[00m' % (str))
+
+  @staticmethod
+  def format_satoshi(satoshi_as_string):
+    if type(satoshi_as_string) == int:
+      satoshi_as_string = str(satoshi_as_string)
+    temp = []
+    for i in range(1, len(satoshi_as_string) + 1):
+      temp.append(satoshi_as_string[-i])
+      if i % 3 == 0 and i != len(satoshi_as_string):
+        temp.append('\'')
+    return ''.join(temp[::-1])
+
+  @staticmethod
+  def open_channels_table(channels):
+    Printer.cprint(
+        ' %12s | %10s | %10s | %11s | %6s | %4s | %10s | %18s | %s' % (
+        'opened at', 'from/to me', 'capacity', 'local_ratio', 'active',
+        'used', 'fwd_events', 'chan_id', 'remote_alias'))
+    for ch in channels:
+      print(' %12s | %10s | %10s | %11.2f | %6s | %4s | %10d | %18s | %s' % (
+            time.strftime('%d %b %H:%M', time.localtime(ch['opened'])),
+            'from me' if ch['outgoing'] else 'to me',
+            Printer.format_satoshi(ch['capacity']),
+            ch['local_ratio'],
+            'yes' if ch['active'] else 'no',
+            'yes' if ch['used'] else 'no',
+            ch['fwd_events'],
+            ch['chan_id'],
+            ch['remote_alias']))
 
 
-class LndToolkit:
+class LndAssistant:
+  def outgoing_channel(self, ch):
+    # TODO: can probably be done for closed channels as well through
+    #       settled_balance and time_locked_balance.
+    return ((float(ch['local_balance']) > 0 and
+             float(ch['total_satoshis_received']) == 0) or
+            (float(ch['total_satoshis_sent']) >
+             float(ch['total_satoshis_received'])))
 
-  def __init__(self):
+
+  def __init__(self, days=7):
     if len(os.popen('lncli walletbalance').read()) == 0:
       raise PermissionError('lncli is locked or does not exist.')
+
+    self.days = days
 
     ## Node info.
     self.my_node_info = json.loads(os.popen('lncli getinfo').read())
@@ -85,7 +114,35 @@ class LndToolkit:
     for edge in self.graph['edges']:
       for pubkey in [edge['node1_pub'], edge['node2_pub']]:
         self.node_stats[pubkey]['channels'] += 1
-        self.node_stats[pubkey]['capacity'] += BTC(edge['capacity'])
+        self.node_stats[pubkey]['capacity'] += sat_to_btc(edge['capacity'])
+
+    ## Forwarding events and routing channels.
+    day = 24 * 60 * 60
+    cmd = ('lncli fwdinghistory --max_events 50000 ' +
+           '--start_time=%d --end_time=%d') % (time.time() - self.days * day,
+                                               time.time() + day)
+    self.fwd_events = json.loads(os.popen(cmd).read())['forwarding_events']
+    # fwd_events: { 'amt_in',
+    #               'amt_out',
+    #               'chan_id_in',
+    #               'chan_id_out',
+    #               'fee',
+    #               'fee_msat',
+    #               'timestamp' }
+    self.chan_id_to_routing_channel = {}
+    for event in self.fwd_events:
+      self.chan_id_to_routing_channel[event['chan_id_in']] = { 'amt_in': [],
+          'amt_out': [], 'fee': [] }
+      self.chan_id_to_routing_channel[event['chan_id_out']] = { 'amt_in': [],
+          'amt_out': [], 'fee': [] }
+    for event in self.fwd_events:
+      self.chan_id_to_routing_channel[event['chan_id_in']]['amt_in'].append(
+          int(event['amt_in']))
+      self.chan_id_to_routing_channel[event['chan_id_out']]['amt_out'].append(
+          int(event['amt_out']))
+      # TODO: maybe use float(event['fee_msat']) / 1000.0
+      self.chan_id_to_routing_channel[event['chan_id_out']]['fee'].append(
+          int(event['fee']))
 
     ## Open channels.
     channels_info = json.loads(os.popen('lncli listchannels').read())
@@ -110,8 +167,35 @@ class LndToolkit:
     #             'total_satoshis_received',
     #             'channel_point',
     #             'local_balance' }
+
+    # Channel opening time (pickled, could be slow the first time called).
+    try:
+      channel_point_to_open_time = pickle.load(open('lnd_assistant.pkl', 'r'))
+    except:
+      channel_point_to_open_time = {}
     for ch in self.channels:
-      ch['outgoing'] = outgoing_channel(ch)
+      if ch['channel_point'] not in channel_point_to_open_time:
+        txid = ch['channel_point'][:ch['channel_point'].find(':')]
+        bitcoind_cmd = 'bitcoin-cli getrawtransaction %s 1' % (txid)
+        txinfo = json.loads(os.popen(bitcoind_cmd).read())
+        channel_point_to_open_time[ch['channel_point']] = txinfo['time']
+    pickle.dump(channel_point_to_open_time, open('lnd_assistant.pkl', 'w'))
+
+    # Additional channel annotations.
+    for ch in self.channels:
+      ch['outgoing'] = self.outgoing_channel(ch)
+      ch['local_ratio'] = float(ch['local_balance']) / float(ch['capacity'])
+      ch['opened'] = channel_point_to_open_time[ch['channel_point']]
+      ch['used'] = ( int(ch['total_satoshis_received']) +
+                     int(ch['total_satoshis_sent']) > 0 )
+      if ch['chan_id'] in self.chan_id_to_routing_channel:
+        routing_channel = self.chan_id_to_routing_channel[ch['chan_id']]
+        ch['fwd_events'] = ( len(routing_channel['amt_in']) +
+                             len(routing_channel['amt_out']) )
+      else:
+        ch['fwd_events'] = 0
+      ch['remote_alias'] = self.node_stats[ch['remote_pubkey']]['alias']
+
     self.chan_id_to_channel = { ch['chan_id'] : ch for ch in self.channels }
     self.remote_pubkey_to_chan_ids = defaultdict(list)
     for ch in self.channels:
@@ -137,10 +221,27 @@ class LndToolkit:
     #                  'BREACH_CLOSE'])
     self.closed_chan_id_to_channel = { ch['chan_id'] : ch
                                        for ch in self.closed_channels }
-    self.closed_channel_points_to_channel = { ch['channel_points'] : ch
+    self.closed_channel_point_to_channel = { ch['channel_point'] : ch
                                               for ch in self.closed_channels }
-    self.remote_pubkey_to_closed_channel_points = defaultdict(list)
+    self.remote_pubkey_to_closed_channel_point = defaultdict(list)
     for ch in self.closed_channels:
-      self.remote_pubkey_to_closed_channel_points[ch['remote_pubkey']].append(
+      self.remote_pubkey_to_closed_channel_point[ch['remote_pubkey']].append(
           ch['channel_point'])
+
+  def newly_opened_channels(self, days=-1):
+    if days == -1:
+      days = self.days
+    threshold = time.time() - 24 * 60 * 60 * days
+    channels = [ch for ch in self.channels if ch['opened'] > threshold]
+    channels = sorted(channels, key=lambda ch: ch['opened'])
+    return channels
+
+  def newly_closed_channels(self, days=-1):
+    if days == -1:
+      days = self.days
+    threshold = self.my_node_info['block_height'] - 144 * days
+    channels = [ ch for ch in self.closed_channels
+                 if ch['close_height'] > threshold ]
+    channels = sorted(channels, key=lambda ch: ch['close_height'])
+    return channels
 
--- report.py
+++ report.py
@@ -1,16 +1,16 @@
 from __future__ import print_function
 import argparse
-from lnd_assistant import LndAssistant, sat_to_btc, format_satoshi
+from lnd_assistant import LndAssistant, sat_to_btc, Printer
 
 
 def print_my_node_info(lnda):
   ## Overall node stats.
-  print(' == My node: %s (%s)' % (lnda.my_node_info['identity_pubkey'],
-      lnda.my_node_info['alias']))
+  Printer.bprint(' == My node: %s (%s)' % (
+      lnda.my_node_info['identity_pubkey'], lnda.my_node_info['alias']))
   print('  URI: %s' % (lnda.my_node_info['uris'][0]))
   print('  LND version: %s' % (lnda.my_node_info['version']))
 
-  print(' == Channels:')
+  Printer.bprint(' == Channels')
   # There are multiple closed channels with chan_id = '0', ignore them.
   closed_channels = [ch for ch in lnda.closed_channels if ch['chan_id'] != '0']
   outgoing_channels = [ch for ch in lnda.channels if ch['outgoing']]
@@ -20,7 +20,7 @@ def print_my_node_info(lnda):
   print('  pending:  %3d' % (lnda.my_node_info['num_pending_channels']))
   print('  closed:   %3d' % (len(closed_channels)))
 
-  print(' == Balance:')
+  Printer.bprint(' == Balance')
   local_balance = sum([ sat_to_btc(ch['local_balance'])
                         for ch in lnda.channels ])
   remote_balance = sum([ sat_to_btc(ch['remote_balance'])
@@ -32,6 +32,22 @@ def print_my_node_info(lnda):
   print('  capacity:    %.5f (local + remote)' % (capacity))
   print('  commit fees: %.5f' % (commit_fees))
   print('  on-chain:    %.5f' % (sat_to_btc(lnda.balance['total_balance'])))
+
+
+def print_opened_and_closed_channels(lnda, days, rows):
+  opened_channels = lnda.newly_opened_channels(days)
+  closed_channels = lnda.newly_closed_channels(days)
+  rows_opened = len(opened_channels) if len(opened_channels) < rows else rows
+  rows_closed = len(closed_channels) if len(closed_channels) < rows else rows
+  Printer.bprint((' == Channels opened in the last %d days ' +
+                  '(showing %d out of %d):') % (days, rows_opened,
+                                                len(opened_channels)))
+  Printer.open_channels_table(opened_channels[-rows:])
+  Printer.bprint((' == Channels closed in the last %d days ' +
+                  '(showing %d out of %d):') % (days, rows_closed,
+                                                len(closed_channels)))
+  for ch in closed_channels[-rows:]:
+    print('')
 
 
 if __name__ == '__main__':
@@ -47,8 +63,9 @@ if __name__ == '__main__':
                       help='List only top rows of tables (default: 20).')
   args = parser.parse_args()
 
-  lnda = LndAssistant()
+  lnda = LndAssistant(days=args.days)
   print_my_node_info(lnda)
+  print_opened_and_closed_channels(lnda, args.days, args.rows)
   ## New channels in the last days.
   ## Channels closed in the last days.
   ## Forwarding stats.
